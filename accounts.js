f33fe22fa57f03ad16020782ed3462bcd161ca2e
cshlxm <cshlxm@users.noreply.github.com>
2023-11-29 21:54:22 +0800

    Add files via upload

---
+++ accounts.js
@@ -0,0 +1,4 @@
+var qweather_key = "2a1a375108944e858e28475bd1686d16";
+var weather_location = "101180101"; // or "116.41,39.92" for GPS coordinate
+var weather_lang = "zh";
+var weather_unit = "m"; // m for meter, i for mile
